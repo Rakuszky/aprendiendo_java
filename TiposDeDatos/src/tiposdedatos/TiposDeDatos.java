2fa5376ffdea9d62571f014c89d33119cbcd5f5e
Rakuszky <leonardonavarro@outlook.cl>
2020-08-15 02:48:29 -0400

    Se creó el archivo TiposdeDatos, en este caso, datos primitivos

---
+++ TiposDeDatos/src/tiposdedatos/TiposDeDatos.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tiposdedatos;
+
+/**
+ *
+ * @author Rakuszky
+ */
+public class TiposDeDatos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        byte entero = 12;
+        int entero2 = 2123456363;
+        float decimal = 4.53f;
+        double decimal2= 34.1231231;
+        char caracter = 's';
+        boolean decision = true;
+        
+        System.out.println("Aquí se presenta un ejemplo de una variable de tipo byte");
+        System.out.println("Número entero: " + entero);
+        System.out.println("");
+        System.out.println("Aquí se presenta una variable de tipo int");
+        System.out.println("Número entero: " + entero2);
+        System.out.println("");
+        System.out.println("Aquí se presenta una variable decimal de tipo float");
+        System.out.println("Número decimal : " + decimal);
+        System.out.println("");
+        System.out.println("Aquí se presenta una variable decimal de tipo double");
+        System.out.println("Número decimal : " + decimal2);
+        System.out.println("");
+        System.out.println("Aquí se presenta una variable decimal de tipo char");
+        System.out.println("Caracter : " + caracter);  
+        System.out.println("");
+        System.out.println("Aquí se presenta una variable decimal de tipo boolean");
+        System.out.println("La desición es : " + decision);
+    }
+    
+}
